2cfeac489d988789cd0f2a16b85daa0c42579ba6
Khwankamon <khwan_yyg@outlook.com>
2024-01-22 18:39:13 +0700

    chang url

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: ["https://deploy-mern-frontend.vercel.app"],
+        origin: ["https://mern-stack-crud-and-search-app.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
